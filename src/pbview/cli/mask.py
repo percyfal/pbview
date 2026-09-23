251c27b3b57b6bf1d65b1585f4ce559ad44d7997
Per Unneberg <per.unneberg@scilifelab.se>
2026-09-23 19:33:22 +0200

    Refactor mask, add set_threads wrapper

--- src/pbview/cli/mask.py
+++ src/pbview/cli/mask.py
@@ -10,15 +10,18 @@ from pathlib import Path
 
 import click
 
-from pbview.logging import app_logger as logger  # noqa
 from pbview.logging import log_level
-from pbview.model.datastore import DataStore
+from pbview.mask import generate as _generate
 
 from ._common import (
+    memory_limit_option,
     path_argument,
     progress_option,
     sampleinfo_option,
+    set_threads,
+    threads_option,
     track_name_option,
+    use_dask_option,
     workers_option,
 )
 
@@ -43,57 +46,37 @@ def mask():
     type=click.Path(dir_okay=True),
     help="Output zarr store",
 )
+@threads_option(default=1)
 @workers_option(default=1)
+@memory_limit_option()
 @track_name_option()
 @log_level()
 @progress_option()
 @sampleinfo_option()
+@use_dask_option()
+@set_threads
 def generate(
     path: Path | str,
     threshold: Path | str,
     output: Path | str,
     track_name: str,
-    workers: int,
     progress: bool,
     sampleinfo: str | None = None,
 ) -> None:
-    """Generate a mask for the pbzarr store."""
-    generate(
+    """Generate masks for the pbzarr store.
+
+    Generate masks for the pbzarr store at PATH based on threshold
+    values in THRESHOLD.
+    """
+    output = output or Path(path).with_name(f"{Path(path).name}_mask")
+    _generate.generate_masks(
         path,
-        threshold,
+        sampleinfo=sampleinfo,
+        threshold=threshold,
         output=output,
         track=track_name,
-        workers=workers,
         progress=progress,
-        sampleinfo=sampleinfo,
     )
 
 
 mask.add_command(generate)
-
-
-def run_create_mask(
-    path: Path | str,
-    threshold: Path | str,
-    output: Path | str | None = None,
-    track: str = "depth",
-    progress: bool = True,
-    sampleinfo: Path | str | None = None,
-) -> None:
-    """Create masks for the pbzarr store.
-
-    Create masks for the pbzarr store at PATH based on threshold
-    values in THRESHOLD.
-    """
-    output = output or Path(path).with_name(f"{Path(path).name}_mask")
-    ds = DataStore(path=path, sampleinfo=sampleinfo)
-    logger.info("Creating mask for track %s based on threshold %s", track, threshold)
-    logger.info("Writing mask to pbzarr store at %s", output)
-    print(ds)
-    # with ProgressBar():
-    #     ds.create_mask(
-    #         track=ds.tracks[track],
-    #         threshold=threshold,
-    #         workers=workers,
-    #         progress=progress,
-    #     )
